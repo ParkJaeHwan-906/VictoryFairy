338e223dbc5b85a52b7baa77d1cc02f8ae8aa61a
Hwannee00 <doormoo2@naver.com>
2026-06-17 17:25:08 +0900

    init: init Project

---
+++ VitoryFairy_BE/src/main/java/com/skhynix/vitoryfairy_be/VitoryFairyBeApplication.java
@@ -0,0 +1,13 @@
+package com.skhynix.vitoryfairy_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitoryFairyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitoryFairyBeApplication.class, args);
+    }
+
+}
